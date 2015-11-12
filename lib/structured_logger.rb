558c548198b901d45b76c51acb282faa957fd103
Peter Marton <martonpe@secretsaucepartners.com>
2015-11-12 17:29:10 +0100

    Add StructuredLogger

--- lib/structured_logger.rb
+++ lib/structured_logger.rb
@@ -1,5 +1,41 @@
 require "structured_logger/version"
+require "logger"
 
-module StructuredLogger
-  # Your code goes here...
+class StructuredLogger
+
+  def initialize(name, log_device=STDOUT)
+    @name = name
+    @logger = ::Logger.new(STDOUT).tap do |logger|
+      logger.formatter = JSONFormatter.new
+    end
+  end
+
+  def event(name, options={})
+    Entry.new(@name, @logger).event(name, options)
+  end
+
+  def metric(name, value, type: "counter")
+    Entry.new(@name, @logger).metric(name, value, type)
+  end
+
+  def debug(message)
+    Entry.new(@name, @logger).debug(message)
+  end
+
+  def info(message)
+    Entry.new(@name, @logger).info(message)
+  end
+
+  def warn(message)
+    Entry.new(@name, @logger).warn(message)
+  end
+
+  def error(message)
+    Entry.new(@name, @logger).error(message)
+  end
+
+  def fatal(message)
+    Entry.new(@name, @logger).fatal(message)
+  end
+
 end
