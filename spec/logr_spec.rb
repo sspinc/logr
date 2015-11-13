d8709e27f479cd0448c756798c16574b2360990d
Peter Marton <martonpe@secretsaucepartners.com>
2015-11-13 17:57:23 +0100

    Add high level tests

---
+++ spec/logr_spec.rb
@@ -0,0 +1,179 @@
+require 'spec_helper'
+require 'logger'
+require 'json'
+
+describe Logr do
+  context 'default DEBUG log level' do
+    let(:test_output) { StringIO.new }
+    let(:logger) { Logr::Logger.new('test_logger', Logger::DEBUG, test_output) }
+
+    describe '#debug' do
+      before do
+        logger.debug('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'DEBUG',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+
+    describe '#info' do
+      before do
+        logger.info('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'INFO',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+
+    describe '#warn' do
+      before do
+        logger.warn('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'WARN',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+
+    describe '#error' do
+      before do
+        logger.error('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'ERROR',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+
+    describe '#fatal' do
+      before do
+        logger.fatal('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'FATAL',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+  end
+
+  context 'WARN debug level' do
+    let(:test_output) { StringIO.new }
+    let(:logger) { Logr::Logger.new('test_logger', Logger::WARN, test_output) }
+
+    describe '#info' do
+      before do
+        logger.info('Test message')
+      end
+
+      it 'should not print anything' do
+        expect(test_output.string).to be_empty
+      end
+    end
+
+    describe '#warn' do
+      before do
+        logger.warn('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'WARN',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+
+    describe '#error' do
+      before do
+        logger.error('Test message')
+      end
+
+      it 'should print a timestamp' do
+        logline = JSON.parse(test_output.string)
+        expect(logline.has_key?('timestamp')).to eq(true)
+      end
+
+      it 'should print everything else in the correct structure' do
+        logline = JSON.parse(test_output.string)
+        logline.delete('timestamp')
+        expected_logline = {
+          'logger'  => 'test_logger',
+          'level'   => 'ERROR',
+          'message' => 'Test message'
+        }
+        expect(logline).to eq(expected_logline)
+      end
+    end
+  end
+end
