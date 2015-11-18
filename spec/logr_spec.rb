72bcc36fa47815ef4a6385db7d749e529d242ab8
Peter Marton <martonpe@secretsaucepartners.com>
2015-11-18 12:13:41 +0100

    DRY up logr_spec

--- spec/logr_spec.rb
+++ spec/logr_spec.rb
@@ -7,113 +7,27 @@ describe Logr do
     let(:test_output) { StringIO.new }
     let(:logger) { Logr::Logger.new('test_logger', level: Logger::DEBUG, log_device: test_output) }
 
-    describe '#debug' do
-      before do
-        logger.debug('Test message')
-      end
+    %w[debug info warn error fatal].each do |level|
+      describe "##{level}" do
+        before do
+          logger.send(level, 'Test message')
+        end
 
-      it 'should print a timestamp' do
-        logline = JSON.parse(test_output.string)
-        expect(logline.has_key?('timestamp')).to eq(true)
-      end
+        it 'should print a timestamp' do
+          logline = JSON.parse(test_output.string)
+          expect(logline.has_key?('timestamp')).to eq(true)
+        end
 
-      it 'should print everything else in the correct structure' do
-        logline = JSON.parse(test_output.string)
-        logline.delete('timestamp')
-        expected_logline = {
-          'logger'  => 'test_logger',
-          'level'   => 'DEBUG',
-          'message' => 'Test message'
-        }
-        expect(logline).to eq(expected_logline)
-      end
-    end
-
-    describe '#info' do
-      before do
-        logger.info('Test message')
-      end
-
-      it 'should print a timestamp' do
-        logline = JSON.parse(test_output.string)
-        expect(logline.has_key?('timestamp')).to eq(true)
-      end
-
-      it 'should print everything else in the correct structure' do
-        logline = JSON.parse(test_output.string)
-        logline.delete('timestamp')
-        expected_logline = {
-          'logger'  => 'test_logger',
-          'level'   => 'INFO',
-          'message' => 'Test message'
-        }
-        expect(logline).to eq(expected_logline)
-      end
-    end
-
-    describe '#warn' do
-      before do
-        logger.warn('Test message')
-      end
-
-      it 'should print a timestamp' do
-        logline = JSON.parse(test_output.string)
-        expect(logline.has_key?('timestamp')).to eq(true)
-      end
-
-      it 'should print everything else in the correct structure' do
-        logline = JSON.parse(test_output.string)
-        logline.delete('timestamp')
-        expected_logline = {
-          'logger'  => 'test_logger',
-          'level'   => 'WARN',
-          'message' => 'Test message'
-        }
-        expect(logline).to eq(expected_logline)
-      end
-    end
-
-    describe '#error' do
-      before do
-        logger.error('Test message')
-      end
-
-      it 'should print a timestamp' do
-        logline = JSON.parse(test_output.string)
-        expect(logline.has_key?('timestamp')).to eq(true)
-      end
-
-      it 'should print everything else in the correct structure' do
-        logline = JSON.parse(test_output.string)
-        logline.delete('timestamp')
-        expected_logline = {
-          'logger'  => 'test_logger',
-          'level'   => 'ERROR',
-          'message' => 'Test message'
-        }
-        expect(logline).to eq(expected_logline)
-      end
-    end
-
-    describe '#fatal' do
-      before do
-        logger.fatal('Test message')
-      end
-
-      it 'should print a timestamp' do
-        logline = JSON.parse(test_output.string)
-        expect(logline.has_key?('timestamp')).to eq(true)
-      end
-
-      it 'should print everything else in the correct structure' do
-        logline = JSON.parse(test_output.string)
-        logline.delete('timestamp')
-        expected_logline = {
-          'logger'  => 'test_logger',
-          'level'   => 'FATAL',
-          'message' => 'Test message'
-        }
-        expect(logline).to eq(expected_logline)
+        it 'should print everything else in the correct structure' do
+          logline = JSON.parse(test_output.string)
+          logline.delete('timestamp')
+          expected_logline = {
+            'logger'  => 'test_logger',
+            'level'   => level.upcase,
+            'message' => 'Test message'
+          }
+          expect(logline).to eq(expected_logline)
+        end
       end
     end
   end
